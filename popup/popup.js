9690be4ab679f4f69b9346d87412e6a4b9a23ee1
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2024-04-10 17:52:39 -0400

    Display time from local storage on popup

---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+const timeDisplay = document.querySelector(".time")
+
+async function getTime() {
+    minutesWorked = await chrome.storage.local.get(["minutesWorked"]);
+    timeDisplay.textContent = `${minutesWorked.minutesWorked} minutes worked`;
+  }
+  
+getTime()

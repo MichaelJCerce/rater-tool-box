51669ae57a26f2c3b0d5d86bf4e8e3f27ec4d587
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2024-05-06 19:23:26 -0400

    Make extension reset at EOD

--- background.js
+++ background.js
@@ -1,9 +1,73 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ totalAET: 0, minutesWorked: 0, task: {} });
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.alarms.create("check-day", {
+    periodInMinutes: 60,
+  });
+
+  await chrome.storage.local.set({
+    totalAET: 0,
+    minutesWorked: 0,
+    task: {},
+    prevWeek: {},
+    currWeek: { totalAET: 0 },
+  });
+
+  await chrome.action.setBadgeText({
+    text: "0.0",
+  });
+});
+
+chrome.alarms.onAlarm.addListener(async () => {
+  const { totalAET, minutesWorked, task, prevWeek, currWeek } =
+    await chrome.storage.local.get([
+      "totalAET",
+      "minutesWorked",
+      "task",
+      "prevWeek",
+      "currWeek",
+    ]);
+
+  if (Object.keys(task).length) {
+    const lastTaskDay = new Date(task.startTime).getDay();
+    const currentDay = new Date().getDay();
+
+    if (lastTaskDay !== currentDay) {
+      currWeek[lastTaskDay] = minutesWorked;
+      currWeek.totalAET += totalAET;
+      if (currentDay === 1) {
+        prevWeek = currWeek;
+        currWeek = { totalAET: 0 };
+      }
+
+      await chrome.storage.local.set({
+        totalAET: 0,
+        minutesWorked: 0,
+        task: {},
+        prevWeek,
+        currWeek,
+      });
+
+      await chrome.action.setIcon({
+        path: {
+          16: "./images/work_time_tracker_16.png",
+          24: "./images/work_time_tracker_24.png",
+          32: "./images/work_time_tracker_32.png",
+        },
+      });
+
+      await chrome.action.setBadgeText({
+        text: "0.0",
+      });
+    }
+  }
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.minutesWorked / request.totalAET > 1.05) {
+  roundedTime = (
+    Math.floor(request.minutesWorked / 60) +
+    Math.ceil((request.minutesWorked % 60) / 6) * 0.1
+  ).toFixed(1);
+
+  if ((+roundedTime * 60) / request.totalAET > 1.09) {
     chrome.action.setIcon({
       path: {
         16: "./images/too_slow_16.png",
@@ -11,7 +75,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         32: "./images/too_slow_32.png",
       },
     });
-  } else if (request.minutesWorked / request.totalAET < 1) {
+  } else if ((+roundedTime * 60) / request.totalAET < 1) {
     chrome.action.setIcon({
       path: {
         16: "./images/too_fast_16.png",
@@ -28,10 +92,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       },
     });
   }
+  
   chrome.action.setBadgeText({
-    text: `${(
-      Math.floor(request.minutesWorked / 60) +
-      Math.ceil((request.minutesWorked % 60) / 6) * 0.1
-    ).toFixed(1)}`,
+    text: `${roundedTime}`,
   });
 });

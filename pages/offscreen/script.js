5fff6f6a2535acbb948865e5eddc23fa0adee373
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2024-05-31 14:07:02 -0400

    Increase volume of alert

--- pages/offscreen/script.js
+++ pages/offscreen/script.js
@@ -1,6 +1,16 @@
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === "alertRater") {
+    const audioCtx = new AudioContext();
     const audio = document.querySelector("audio");
+
+    const source = audioCtx.createMediaElementSource(audio);
+    const gainNode = audioCtx.createGain();
+
+    gainNode.gain.value = 4;
+
+    source.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
     audio.play();
   }
 });
